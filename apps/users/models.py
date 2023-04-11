689d59b395a6c140051903ba251776e71314c127
choc1403 <eloicx@gmail.com>
2023-04-11 17:55:17 -0600

    'init'

---
+++ apps/users/models.py
@@ -0,0 +1,21 @@
+# ORM DE DJANGO
+from django.db import models
+
+# MODELO
+from django.contrib.auth.models import AbstractUser
+
+# SETTINGS OF PROJECT
+from proyecto.settings import MEDIA_URL, STATIC_URL
+
+# Create your models here.
+class User(AbstractUser):
+    imagen = models.ImageField(upload_to='users/', null=True, blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} {} / {}'.format(self.first_name, self.last_name, self.username)
+    
+    def get_image(self):
+        if self.image:
+            return '{}{}'.format(MEDIA_URL, self.image)
+        return '{}{}'.format(STATIC_URL, 'img/usuario.png')
---
+++ proyecto/urls.py
@@ -0,0 +1,20 @@
+# URL PARA ACCEDER AL ADMIN DE DJANGO
+from django.contrib import admin
+
+# AGREGAR URLS
+from django.urls import path, include
+
+# VISTAS
+from . import views
+
+# CONFIGURANCION PARA MANEJAR LOS STATICS Y MEDIA
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('admin/', admin.site.urls),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:    
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
