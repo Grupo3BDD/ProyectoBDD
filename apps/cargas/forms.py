2e1ebbc7e651c7db375e0d60e6c0ac811f0e7bfc
choc1403 <eloicx@gmail.com>
2023-05-04 21:36:12 -0600

    Cargas, view, url, forms

---
+++ apps/cargas/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms import ModelForm, DateInput, NumberInput
+from .models import Carga
+
+class CargaForm(ModelForm):
+    class Meta: 
+        model = Carga
+        fields = '__all__'
+        widgets = {
+            'year': NumberInput(attrs = {'type': 'number'}),
+            'fecha_envio': DateInput(attrs = {'type': 'date'}),
+            'fecha_aprob': DateInput(attrs = {'type': 'date'})
+        }
+
---
+++ apps/cargas/views.py
@@ -0,0 +1,56 @@
+from typing import Any
+from django.db import models
+from django.db.models.query import QuerySet
+from django.shortcuts import render, redirect, reverse
+from django.views import generic
+from .models import Carga
+from .forms import CargaForm
+from django.views.generic import View
+from .utils import breadcrumb
+
+
+class CargaListView(generic.ListView):
+    template_name = "cargas/carga_list.html"    
+    queryset = Carga.objects.all().order_by('-id')
+    paginate_by = 5
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        
+        context['message'] = 'Administración de Cargas'
+        context['title'] = 'Cargas'
+        context['breadcrumb'] = breadcrumb()
+
+        return context
+    
+
+class CargaCreateView(generic.CreateView):
+    template_name = "cargas/carga_create.html"
+    form_class = CargaForm
+
+    def get_success_url(self):
+        return reverse("cargas:carga-list")
+    
+    def get_queryset(self):
+        return Carga.objects.all()
+    
+class CargaDeleteView(generic.DeleteView):
+    template_name = "cargas/carga_delete.html"
+
+    def get_success_url(self):
+        return reverse("cargas:carga-list")
+    
+    def get_queryset(self):
+        return Carga.objects.all()
+    
+
+class CargaUpdateView (generic.UpdateView):
+    template_name = "cargas/carga_update.html"
+    form_class = CargaForm
+
+    def get_success_url(self):
+        return reverse("cargas:carga-list")
+    
+    def get_queryset(self):
+        return Carga.objects.all()
+    
